b9f9ce24300f44168babb122fe11f9b8e1db8ea0
Oleksiy Atamaniuk <alexata1204@gmail.com>
2021-07-11 20:19:30 +0300

    [AWS-Task-9] Backend For Frontend

---
+++ bff-service/src/app.module.ts
@@ -0,0 +1,11 @@
+import { CacheModule, HttpModule, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [ConfigModule.forRoot(), HttpModule, CacheModule.register()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
